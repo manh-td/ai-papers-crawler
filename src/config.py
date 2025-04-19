8a3f4cd0f1093992ab9c4700d5935de2980fced7
manh-td <dmtran120901@gmail.com>
2025-04-19 20:52:59 +0800

    Update year range logic and uncomment conference URLs in config

--- src/config.py
+++ src/config.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+
 LOGS_DIR = "./logs"
 OUTPUT_DIR = "./outputs"
 PRODUCT = True
 
-# YEARS = list(range(2024, 2017, -1))
-YEARS = ["2024"]
+current_year = datetime.now().year
+YEARS = list(range(current_year, 2017, -1))
 
 CONFERENCE_LIST = [
-    # ("iclr", "https://iclr.cc/virtual/{year}/papers.html?layout=detail"),
+    ("iclr", "https://iclr.cc/virtual/{year}/papers.html?layout=detail"),
     ("neurips", "https://neurips.cc/virtual/{year}/papers.html?layout=detail"),
-    # ("icml", "https://icml.cc/virtual/{year}/papers.html?layout=detail"),
+    ("icml", "https://icml.cc/virtual/{year}/papers.html?layout=detail"),
     ("emnlp", "https://{year}.emnlp.org/program/accepted_main_conference/")
 ]
