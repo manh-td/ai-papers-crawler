2023db275c91fd88107ac0b53f9596f05f9de770
manh-td <dmtran120901@gmail.com>
2025-05-24 10:43:19 +0800

    Update requirements and refactor topic extraction; integrate BERTopic and enhance logging

--- src/topic_extractor.py
+++ src/topic_extractor.py
@@ -4,35 +4,16 @@ from transformers import (
     AutoTokenizer,
     set_seed,
 )
-from .utils import load_jsonl, write_json
+from .utils import load_jsonl, write_json, logging
 from .config import (
     ALL_PAPERS_DIR,
     OUTPUT_DIR
 )
 from tqdm import tqdm  # Import tqdm for progress tracking
-
-class KeyphraseGenerationPipeline(Text2TextGenerationPipeline):
-    def __init__(self, model, keyphrase_sep_token=";", *args, **kwargs):
-        super().__init__(
-            model=AutoModelForSeq2SeqLM.from_pretrained(model, device_map="cuda:1"),
-            tokenizer=AutoTokenizer.from_pretrained(model, device_map="cuda:1"),
-            *args,
-            **kwargs
-        )
-        self.keyphrase_sep_token = keyphrase_sep_token
-
-    def postprocess(self, model_outputs):
-        results = super().postprocess(
-            model_outputs=model_outputs
-        )
-        return [[keyphrase.strip() for keyphrase in result.get("generated_text").split(self.keyphrase_sep_token) if keyphrase != ""] for result in results]
+from bertopic import BERTopic
 
 # Set seed for reproducibility
 set_seed(42)
-
-# Load pipeline
-model_name = "ml6team/keyphrase-generation-keybart-inspec"
-generator = KeyphraseGenerationPipeline(model=model_name)
 
 if __name__ == "__main__":
     papers = load_jsonl(ALL_PAPERS_DIR)
@@ -42,13 +23,16 @@ if __name__ == "__main__":
         title = paper["title"]
         titles.append(title)
 
-    keyphrases = generator(titles)  # Add tqdm here
+    topic_model = BERTopic()
+    topics, probs = topic_model.fit_transform(titles)
+
+    logging.info(topic_model.get_topic_info())
 
     # Create a dictionary to count the appearance of each topic
-    topic_appearance = {}
-    for keyphrase_list in tqdm(keyphrases, desc="Counting topic appearances"):  # Add tqdm here
-        for topic in keyphrase_list:
-            topic_appearance[topic] = topic_appearance.get(topic, 0) + 1
+    # topic_appearance = {}
+    # for keyphrase_list in tqdm(keyphrases, desc="Counting topic appearances"):  # Add tqdm here
+    #     for topic in keyphrase_list:
+    #         topic_appearance[topic] = topic_appearance.get(topic, 0) + 1
 
-    topic_appearance = dict(sorted(topic_appearance.items(), key=lambda item: item[1], reverse=True))
-    write_json(f"{OUTPUT_DIR}/topics.jsonl", topic_appearance)
+    # topic_appearance = dict(sorted(topic_appearance.items(), key=lambda item: item[1], reverse=True))
+    # write_json(f"{OUTPUT_DIR}/topics.jsonl", topic_appearance)
